d29c78f40453d8a24d7268821683385d2e4fcc13
daky chanthavong <daki.litd@gmail.com>
2024-08-27 11:38:08 +0700

    Read the data from the transport layer and close the connection after writing it back

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
-	"time"
 )
 
 func main() {
@@ -27,6 +29,43 @@ func execute() error {
 	/   (_____/
 	/_____/   U Server is running at port 8080...`)
 
-	time.Sleep(1 * time.Second)
-	return nil
+	for {
+		// Waits for connection from listening.
+		conn, err := l.Accept()
+		if err != nil {
+			return fmt.Errorf("failed to next connection to the listener: %w", err)
+		}
+		defer conn.Close()
+
+		// Wrap the connection in a buffered reader.
+		reader := bufio.NewReader(conn)
+
+		b := make([]byte, 0, 4)
+		for {
+			// starting at the end of the current slice b and extending to its full capacity.
+			n, err := reader.Read(b[len(b):cap(b)])
+			// resizes the slice b to include the bytes that were just read.
+			b = b[:len(b)+n]
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				return err
+			}
+
+			log.Println("number of bytes read into p: ", n, " and current length of b: ", len(b))
+			if len(b) == cap(b) {
+				// Add more capacity (let append pick how much).
+				// and unchanged the original length.
+				b = append(b, 0)[:len(b)]
+				log.Println("Extended Cap: ", cap(b))
+				continue
+			}
+			break
+		}
+
+		log.Println(string(b))
+		conn.Write([]byte("hello world!"))
+		return nil
+	}
 }
